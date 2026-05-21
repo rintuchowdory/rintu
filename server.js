ab79fea7c1bedd346c41ca8c7aa26fe5fafb76ed
rintuchowdory <chowdoryrintu60@gmail.com>
2026-05-21 20:38:19 +0200

    feat: add static Express server and Procfile for Render deployment

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const root = path.join(__dirname);
+app.use(express.static(root));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(root, "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
